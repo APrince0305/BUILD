6dcdbc101e534b116566ee0188d99e5a330f2cef
APrince0305 <134970765+APrince0305@users.noreply.github.com>
2026-08-17 04:17:50 +0000

    add map

--- ne-ohio-sites/components/Map.tsx
+++ ne-ohio-sites/components/Map.tsx
@@ -1,13 +1,25 @@
 'use client'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 
 export default function Map() {
+  const [counties, setCounties] = useState(null)
+
+  useEffect(() => {
+    fetch('/data/neo_counties.json')
+      .then((res) => res.json())
+      .then((data) => setCounties(data))
+  }, [])
+
   return (
     <MapContainer center={[41.4, -81.7]} zoom={9} style={{ height: '600px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; OpenStreetMap contributors'
       />
+      {counties && (
+        <GeoJSON data={counties} style={{ color: '#2563eb', weight: 2, fillOpacity: 0.05 }} />
+      )}
     </MapContainer>
   )
 }
